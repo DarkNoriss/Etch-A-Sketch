3300985eb5a90b009bb227a0405610d0af5e74ac
DarkNoriss <darknoriss@gmail.com>
2023-03-02 17:57:51 +0100

    done

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
-const mainDiv = document.querySelector('#app')
+const mainBody = document.querySelector('body')
 const newPaintboardButton = document.querySelector('#newPaintBoard')
+const mainDiv = document.querySelector('#app')
+let divItems = []
 
-drawPaintboard(16)
+
+drawPaintboard(100)
 
 function drawPaintboard(a) {
   let howManydivs = a
+  let colId = 0;
   ///let divsSquereRoot = Math.sqrt(howManydivs);
 
   for (let startRow = 0; startRow < howManydivs; startRow++) {
     let newDivRow = document.createElement('div')
     newDivRow.classList.add('app__row'+ startRow +'')
-  
+
     for (let startCol = 0; startCol < howManydivs; startCol++) {
+      
       let newDivCol = document.createElement('div')
-      newDivCol.classList.add('app__col'+ startCol +'')
+      newDivCol.classList.add('app__col' + colId + '')
+      
       newDivRow.appendChild(newDivCol)
+      colId++
     }
-  
     mainDiv.appendChild(newDivRow)
   }
+  divItems = document.querySelectorAll(`[class*="app__col"]`);
+  listen()
 }
 
 function deletePaintboard() {
@@ -26,10 +34,12 @@ function deletePaintboard() {
 }
 
 function createNewPaintboard() {
-  let howBig = prompt('How big u want the paint board to be?');
+  let howBig = parseInt(prompt('How big u want the paint board to be?'));
 
-  if (howBig > 100) {
+  if (howBig > 900) {
     alert('U cant make paint board bigger than 200!')
+  } else if (howBig < 0 || howBig == null || howBig == '' || isNaN(howBig)) {
+    alert('Paint board must be bigger than 0!')
   } else {
     deletePaintboard()
     drawPaintboard(howBig)
@@ -38,4 +48,28 @@ function createNewPaintboard() {
 
 newPaintboardButton.addEventListener("click", (e) => {
   createNewPaintboard()
-});
+})
+
+function listen() {
+  for (let i = 0; i < divItems.length; i++){
+    divItems[i].addEventListener('mousedown', drawClick);
+    divItems[i].addEventListener('mouseenter', drawClickHover);
+  }
+}
+
+function drawClick(e) {
+  e.target.style.backgroundColor = 'black'
+}
+
+function drawClickHover(e) {
+  if (e.buttons > 0) {
+    e.target.style.backgroundColor = 'black'
+  }
+}
+
+
+
+
+
+
+
